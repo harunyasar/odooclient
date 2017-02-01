2e0126ccaecd277556fa3e37051f4fc86e61d795
Joël Gaujard <joel@depiltech.com>
2017-02-01 18:27:58 +0100

    Add Unit test for name_get method

--- tests/ReadTest.php
+++ tests/ReadTest.php
@@ -98,44 +98,4 @@ class ReadTest extends TestOdooClient
         $this->assertEquals(count($myIds), count($result));
     }
 
-
-
-
-    /*
-    public function testPagination()
-    {
-        $limit = 5;
-
-        $parameters = array(
-            'offset' => new xmlrpcval(10, xmlrpcval::$xmlrpcInt),
-            'limit' => new xmlrpcval($limit, xmlrpcval::$xmlrpcInt),
-        );
-
-        $client = new OdooClient($this->_host, $this->_port, $this->_db, $this->_username, $this->_password);
-        $domain = array(new xmlrpcval(array(), xmlrpcval::$xmlrpcArray));
-        $ids = $client->search($this->_model_name, $domain, $parameters);
-
-        $this->assertTrue(is_array($ids));
-        $this->assertEquals($limit, count($ids));
-    }
-
-    public function testFilter()
-    {
-        $restriction = array(
-            new xmlrpcval('code', xmlrpcval::$xmlrpcString),
-            new xmlrpcval('=', xmlrpcval::$xmlrpcString),
-            new xmlrpcval('US', xmlrpcval::$xmlrpcString)
-        );
-        $domain = array(new xmlrpcval($restriction, xmlrpcval::$xmlrpcArray));
-
-        $client = new OdooClient($this->_host, $this->_port, $this->_db, $this->_username, $this->_password);
-        $domain = array(new xmlrpcval($domain, xmlrpcval::$xmlrpcArray));
-        $ids = $client->search($this->_model_name, $domain);
-
-        $this->assertTrue(is_array($ids));
-        $this->assertEquals(1, count($ids));
-        $this->assertArrayHasKey(0, $ids);
-        $this->assertEquals($ids[0], 235);
-    }
-*/
 }
